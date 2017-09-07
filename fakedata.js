a9e0797f228d95779e0d512a2c9219aca4bfe4c9
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-07 11:36:25 -0700

    Add starter data and start of Selection component

--- fakedata.js
+++ fakedata.js
@@ -1,3 +1,37 @@
-// description from https://en.wikipedia.org/wiki/Lavandula_angustifolia: 
-// English lavender is commonly grown as an ornamental plant. It is popular for its colourful flowers, its fragrance, and its ability to survive with low water consumption.
-// lavender img: http://thegraphicsfairy.com/wp-content/uploads/2013/08/Lavender-Botanical-Printable-GraphicsFairysm.jpg
+from http://thegraphicsfairy.com/free-vintage-flower-images/
+
+description from: https://en.wikipedia.org/wiki/Lavandula_angustifolia: 
+English lavender is commonly grown as an ornamental plant. It is popular for its colourful flowers, its fragrance, and its ability to survive with low water consumption.
+lavender img: http://thegraphicsfairy.com/wp-content/uploads/2013/08/Lavender-Botanical-Printable-GraphicsFairysm.jpg
+genus: Lavandula
+species: Angustifolia
+
+name: Camellia
+genus: Camellia
+species: Japonica
+Camellia Madame P. de Pannemaeker
+description from: https://en.wikipedia.org/wiki/Camellia
+Camellias are found in eastern and southern Asia, from the Himalayas east to Japan and Indonesia. There are 100–300 described species, with some controversy over the exact number. There are also around 3,000 hybrids.
+img url: http://thegraphicsfairy.com/wp-content/uploads/blogger/_CarNcodpCMA/TK6Dlcez5II/AAAAAAAAJUQ/1xVRliaxLTM/s1600/camellia-graphicsfairy005bsm.jpg
+
+name: Amaryllis
+genus: Hippeastrum
+species: Reginae
+description from: https://en.wikipedia.org/wiki/Hippeastrum
+description: The name Hippeastrum, given to it by William Herbert, means "Knight's-star-lily", although precisely what Herbert meant by the name is not certain. For many years there was confusion among botanists over the generic names Amaryllis and Hippeastrum, one result of which is that the common name "amaryllis" is mainly used for cultivars of this genus, often sold as indoor flowering bulbs particularly at Christmas in the northern hemisphere. By contrast the generic name Amaryllis applies to bulbs from South Africa, usually grown outdoors. 
+img url: http://thegraphicsfairy.com/wp-content/uploads/2013/11/Free-Christmas-Printables-Amaryllis-GraphicsFairy-sm.jpg
+
+name: Egyptian Water Lily
+genus: Nymphaea
+species: Pubescens
+description from: https://en.wikipedia.org/wiki/Nymphaea_pubescens
+description: The leaves of this plant have fuzzy or hairy undersides and the stems are covered by the same hairs as well, hence the name "pubescens" or "hairy" of the species. This is not a characteristic that is apparent when looking at the plant from above the water though.
+img url: 'http://thegraphicsfairy.com/wp-content/uploads/2013/04/Vintage-Printable-Water-Lily-GraphicsFairy-sm3.jpg'
+
+name: Oxeye Daisy
+genus: Leucanthemum
+species: Vulgare
+description from: https://en.wikipedia.org/wiki/Leucanthemum_vulgare
+description: This is a widespread flowering plant native to Europe and the temperate regions of Asia and an introduced plant to North America, Australia and New Zealand. It is one of a number of family Asteraceae plants to be called a "daisy", and has the additional vernacular names common daisy, dog daisy and moon daisy.
+img url: http://thegraphicsfairy.com/wp-content/uploads/blogger/-muCu66Md59g/ToJcuJvaE9I/AAAAAAAAONE/0jvZlNAtdxM/s1600/daisy%2Bvintage%2Bimage%2Bgraphicsfairy005sm.jpg
+
