ed1f762d0ad8accdbc0f0e56e039a6da30191053
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-09-11 11:09:46 -0700

    Auth reducers tests pass

--- src/root-reducer.js
+++ src/root-reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { cards, fetchHasErrored, cardsAreLoading } from './store/card.reducers';
+import auth from './store/auth.reducers';
 import { selection } from './store/selection.reducers';
 
 export default combineReducers({
-    cards: cards,
-    selection: selection,
-    fetchHasErrored: fetchHasErrored,
-    cardsAreLoading: cardsAreLoading
+    auth,
+    cards,
+    selection,
+    fetchHasErrored,
+    cardsAreLoading
 });
