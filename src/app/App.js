92cbcde94b06bb62020211550e8d22082e1ac4d4
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-11-03 14:03:27 -0700

    Finished rearrangement into feature-based file organization

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './rose-icon.png';
+import logo from '../rose-icon.png';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { connect } from 'react-redux';
--- src/selection/Selection.test.js
+++ src/selection/Selection.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJSON from 'enzyme-to-json';
 
-import { Selection } from '../components/Selection';
+import { Selection } from './Selection';
 
 describe('Selection Component Snapshot', () => {
 
